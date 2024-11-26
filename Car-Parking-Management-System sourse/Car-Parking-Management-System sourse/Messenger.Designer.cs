2f1cd0ce2834e95de90c586e7a7f2986a61f2c7d
thanhtanmt <tanle20022005@gmail.com>
2024-11-26 09:46:48 +0700

    tanle update messenger

--- Car-Parking-Management-System sourse/Car-Parking-Management-System sourse/Messenger.Designer.cs
+++ Car-Parking-Management-System sourse/Car-Parking-Management-System sourse/Messenger.Designer.cs
@@ -29,15 +29,16 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.name_user = new System.Windows.Forms.Label();
+            this.text_input = new System.Windows.Forms.TextBox();
             this.pictureBox5 = new System.Windows.Forms.PictureBox();
-            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.send_message = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.flowLayoutPanelMessages = new System.Windows.Forms.FlowLayoutPanel();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.send_message)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -53,24 +54,23 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(0, 54);
             this.label1.TabIndex = 2;
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // label2
+            // name_user
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(140, 35);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(51, 20);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "label2";
+            this.name_user.AutoSize = true;
+            this.name_user.Location = new System.Drawing.Point(140, 35);
+            this.name_user.Name = "name_user";
+            this.name_user.Size = new System.Drawing.Size(51, 20);
+            this.name_user.TabIndex = 3;
+            this.name_user.Text = "label2";
             // 
-            // textBox1
+            // text_input
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 654);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(379, 62);
-            this.textBox1.TabIndex = 7;
+            this.text_input.Location = new System.Drawing.Point(12, 654);
+            this.text_input.Multiline = true;
+            this.text_input.Name = "text_input";
+            this.text_input.Size = new System.Drawing.Size(379, 62);
+            this.text_input.TabIndex = 7;
             // 
             // pictureBox5
             // 
@@ -83,15 +83,16 @@
             this.pictureBox5.TabIndex = 9;
             this.pictureBox5.TabStop = false;
             // 
-            // pictureBox4
+            // send_message
             // 
-            this.pictureBox4.Image = global::Car_Parking_Management_System_sourse.Properties.Resources.image_removebg_preview;
-            this.pictureBox4.Location = new System.Drawing.Point(427, 654);
-            this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(67, 62);
-            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox4.TabIndex = 8;
-            this.pictureBox4.TabStop = false;
+            this.send_message.Image = global::Car_Parking_Management_System_sourse.Properties.Resources.image_removebg_preview;
+            this.send_message.Location = new System.Drawing.Point(427, 654);
+            this.send_message.Name = "send_message";
+            this.send_message.Size = new System.Drawing.Size(67, 62);
+            this.send_message.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.send_message.TabIndex = 8;
+            this.send_message.TabStop = false;
+            this.send_message.Click += new System.EventHandler(this.send_message_Click);
             // 
             // pictureBox3
             // 
@@ -111,7 +112,6 @@
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 4;
             this.pictureBox2.TabStop = false;
-            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
             // 
             // pictureBox1
             // 
@@ -122,17 +122,25 @@
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
+            // flowLayoutPanelMessages
+            // 
+            this.flowLayoutPanelMessages.Location = new System.Drawing.Point(0, 124);
+            this.flowLayoutPanelMessages.Name = "flowLayoutPanelMessages";
+            this.flowLayoutPanelMessages.Size = new System.Drawing.Size(506, 510);
+            this.flowLayoutPanelMessages.TabIndex = 10;
+            // 
             // Messenger
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(506, 728);
+            this.Controls.Add(this.flowLayoutPanelMessages);
             this.Controls.Add(this.pictureBox5);
-            this.Controls.Add(this.pictureBox4);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.send_message);
+            this.Controls.Add(this.text_input);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.name_user);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -140,7 +148,7 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Messenger";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.send_message)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -153,11 +161,12 @@
 
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label name_user;
         private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.TextBox text_input;
+        private System.Windows.Forms.PictureBox send_message;
         private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanelMessages;
     }
 }
--- Car-Parking-Management-System sourse/Car-Parking-Management-System sourse/Messenger.cs
+++ Car-Parking-Management-System sourse/Car-Parking-Management-System sourse/Messenger.cs
@@ -7,29 +7,124 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Car_Parking_Management_System_sourse
 {
     public partial class Messenger : Form
     {
-        public Messenger()
+        private string filePath;
+        private string id_customer;
+        private string id_attendant;
+        bool flag;
+        public Messenger(string id_customer, string id_attendant, bool flag)
         {
             InitializeComponent();
+            filePath = $"{id_customer}_{id_attendant}.txt";
+            this.flag = flag;
+            this.id_attendant = id_attendant;
+            this.id_customer= id_customer;
+            name_user.Text = flag ? id_attendant : id_customer;
+            LoadMessages();
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void send_message_Click(object sender, EventArgs e)
         {
+            string message = text_input.Text.Trim();
+
+            if (!string.IsNullOrEmpty(message))
+            {
+                string senderId = flag ? id_attendant : id_customer;
+                string formattedMessage = $"[{DateTime.Now:yyyy-MM-dd HH:mm:ss}] {senderId}: {message}";
+
+                File.AppendAllText(filePath, formattedMessage + Environment.NewLine);
+
+                AddMessageToPanel(formattedMessage, flag);
+
+                text_input.Clear();
+            }
 
         }
 
-        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        private void LoadMessages()
         {
+            flowLayoutPanelMessages.Controls.Clear();
 
+            if (File.Exists(filePath))
+            {
+                string[] lines = File.ReadAllLines(filePath);
+
+                foreach (string line in lines)
+                {
+                    bool isCustomer = line.Contains($"{id_customer}:");
+                    AddMessageToPanel(line, isCustomer);
+                }
+            }
         }
+        //private void AddMessageToPanel(string message, bool isCustomer)
+        //{
+        //    Label messageLabel = new Label
+        //    {
+        //        Text = message,
+        //        AutoSize = true,
+        //        Padding = new Padding(10),
+        //        Margin = new Padding(5),
+        //        MaximumSize = new Size(flowLayoutPanelMessages.Width - 30, 0), 
+        //    };
 
-        private void pictureBox2_Click(object sender, EventArgs e)
+        //    if (isCustomer)
+        //    {
+        //        messageLabel.BackColor = Color.LightBlue;
+        //        messageLabel.TextAlign = ContentAlignment.MiddleRight;
+        //        messageLabel.Anchor = AnchorStyles.Right;
+        //    }
+        //    else
+        //    {
+        //        messageLabel.BackColor = Color.LightGreen;
+        //        messageLabel.TextAlign = ContentAlignment.MiddleLeft;
+        //        messageLabel.Anchor = AnchorStyles.Left;
+        //    }
+
+        //    flowLayoutPanelMessages.Controls.Add(messageLabel);
+
+        //    flowLayoutPanelMessages.ScrollControlIntoView(messageLabel);
+        //}
+        private void AddMessageToPanel(string message, bool isCustomer)
         {
+            Panel messagePanel = new Panel
+            {
+                AutoSize = true,
+                Padding = new Padding(10),
+                Margin = new Padding(5),
+                BackColor = isCustomer ? Color.LightBlue : Color.LightGreen, // Màu nền
+                MaximumSize = new Size(flowLayoutPanelMessages.Width - 50, 0), // Giới hạn chiều rộng
+            };
 
+            Label messageLabel = new Label
+            {
+                Text = message,
+                AutoSize = true,
+                Dock = DockStyle.Fill,
+                ForeColor = Color.Black, // Màu chữ
+            };
+
+            // Thêm Label vào Panel
+            messagePanel.Controls.Add(messageLabel);
+
+            if (isCustomer)
+            {
+                messagePanel.Anchor = AnchorStyles.Right;
+            }
+            else
+            {
+                messagePanel.Anchor = AnchorStyles.Left; 
+            }
+
+            flowLayoutPanelMessages.Controls.Add(messagePanel);
+
+            flowLayoutPanelMessages.ScrollControlIntoView(messagePanel);
         }
+
     }
 }
+
